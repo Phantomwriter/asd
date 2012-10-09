282e69b696c94f79766c3531225f5f6893a86043
Phantomwritergit config --global user.nametakeovertheworld.livingston7@gmail.comgit config --global user.name takeovertheworld.livingston7@gmail.com <takeovertheworld.livingston7git config --global user.name takeovertheworld.livingston7@gmail.com>
2012-10-09 01:38:35 -0400

    Git commit of screenshot with eclipse and couch

---
+++ Wolflinx/js/cards.js
@@ -0,0 +1,60 @@
+/*
+This is cards.js
+
+A List of Yu-Gi-Oh Game cards for trading
+
+*/
+var jsonCards = {
+	"Monster Cards" :{
+		"cName":["Name:","Caius The Shadow Monarch"],
+		"cAttack":["Attack:","2400"],
+		"cDef":["Defense:","1000"],
+		"cEffect":["Card Effect:","When this card is Normal summopned successfully, select one card on the field and remove it from play. If it is a dark monster, inflict 1000 points of damage to your opponents life points"],
+		"cRules":["Card Rules:","This card's effect is chainable. The option to remove a card from play is not optional. If Caius is the only card on the field when it's summoned, it must remove itself from the game and it's effect is reolved in a chain after this effect is applied"],
+		"cLevel":["Cards Star level","6"],
+		"cQuantity":["How many in the deck","2"],
+
+	},	
+	
+	"Monster Cards" :{
+		"cName":["Name:","Mobius The Frost Monarch"],
+		"cAttack":["Attack:","2400"],
+		"cDef":["Defense:","1000"],
+		"cEffect":["Card Effect:","When this card is Normal summopned successfully, select and destroy up to two spell or trap cards on the field"],
+		"cRules":["Card Rules:","This card's effect is chainable. The option to destroy ANY cards is optional."],
+		"cLevel":["Cards Star level","6"],
+		"cQuantity":["How many in the deck","side-deck"],
+
+	},	
+	
+	"Monster Cards" :{
+		"cName":["Name:","Chaos Sorcerer"],
+		"cAttack":["Attack:","2300"],
+		"cDef":["Defense:","2000"],
+		"cEffect":["Card Effect:","This card can only be special summoned by removing 1 light monster and 1 dark monster from your graveyard from play.once during each of your turns, you can remove 1 face-up monster on the field from play. This monster cannot attack this turn if this effect is activated."],
+		"cRules":["Card Rules:","This card's effect is chainable. The option to remove a monster from play is optional. If Chaos sorcerer removes a card from play, is flipped face-down by a card effect, then flipped face-up, it can remove another monster on the field from play because it's conditions were reset"],
+		"cLevel":["Cards Star level","6"],
+		"cQuantity":["How many in the deck","1"],
+
+	},	
+	
+	
+	"Monster Cards" :{
+		"cName":["Name:","Cyber Dragon"],
+		"cAttack":["Attack:","2100"],
+		"cDef":["Defense:","1600"],
+		"cEffect":["Card Effect:","If you control no monsters, you can special summoj this monster from your hand"],
+		"cRules":["Card Rules:","This card is considered a special summon"],
+		"cLevel":["Cards Star level","5"],
+		"cQuantity":["How many in the deck","3"],
+
+	},	
+	
+	"Monster Cards" :{
+		"cName":["Name:","Morphing jar"],
+		"cAttack":["Attack:","700"],
+		"cDef":["Defense:","600"],
+		"cEffect":["Card Effect-Flip:"," When this card is flip-summoned, both players must dicard their entire hand, then each player draws five cards."],
+		"cRules":["Card Rules:","This card's effect is chainable. If the cards are removed from play when discarded, the players both still get the effect of drawing five cards."],
+		"cLevel":["Cards Star level",2"],
+	},	
--- Wolflinx/js/main.js
+++ Wolflinx/js/main.js
@@ -4,261 +4,108 @@
 Howard Livingston
 ASD with Marianne Sheldon
 9/12
-JS page with Jquery
+mainJS page with Jquery
 
 ///////////////////////////////////////////////////
 */
 
 
 //Laod Page
-
 $('#home').on('pageinit', function(){
+		console.log("It's working");
+		/* then*/
+});	
 
-	
+
+//SignUp Page
 $('#signupPage').on('pageinit', function(){
-
-		var myForm=$('#contactForm');
+	alert("It's also working");
+	var validate = function(){
+	var myForm=$('#signupForm');
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
 			},
 			submitHandler:function(){
-		var data = myForm.serializeArray();
+	var data = myForm.serializeArray();
 			storeData(data);
+
 		}
 	});
-	
+	}
+
+//xmlPage
+$('#xmlButton').on('click',function(){
+                $('#showData').empty();
+                $('<p>').html('XML Data Imported').appendTo('#xmlPage');
+                $.ajax({
+                    url:"xhr/data.xml",
+                    type: 'GET',
+                    dataType: 'xml',
+                    success: function(xml){
+                       console.log(xml);
+                       $(xml).find("item").each(function(){
+						var firstName = $(this).find('fname').text();
+						var lastName = $(this).find('lname').text();
+						var password = $(this).find('pword').text();
+						var confirmPassword = $(this).find('cpword').text();
+						var eMail = $(this).find('email').text();
+						var deviceValue = $(this).find('deviceValue').text();
+						var friends = $(this).find('friends').text();
+						var group = $(this).find('group').text();
+						var date = $(this).find('date').text();
+
+                            $(''+
+								  '<div id="items" style="padding:10px" data-role="fieldcontain">'+
+										'<p>First name: '+ fname +'</p>'+
+										'<p>LastName: '+ lname +'</p>'+
+										'<p>Password: '+ pword +'</p>'+
+										'<p>Confirm password: '+ cpword+'</p>'+
+										'<p>E-Mail: '+ email +'</p>'+
+										'<p>deviceValue: '+ devicevalue +'</p>'+
+										'<p>friends: '+ friends +'</p>'+
+										'<p>group: '+ group +'</p>'+
+										'<p>date: '+ date +'</p>'+
+								   '</div>'
+                                 ).appendTo('#xmlPage');
+                 	  })
+				 }
+			})
+		});
 
 
-//shotrten console.log into just......say().
-		var say = function(message){
-		         console.log(message);
-};	
+});	
 
-
-//Get the radio button data			
-
-	function getSelectedRadio() {
-		var radio = document.forms[0].deviceValue;
-		for (var i=0; i<radio.length; i++){
-			if(radio[i].checked){
-				deviceValue = radio[i].value;
+//get json data with ajax
+$('#jsonButton').on('click', function(){
+		$("#jsonData").empty();
+		$.ajax({
+			url:'xhr/data.json',
+			type:'GET',
+			dataType:'json',
+			success:function(response){
+				console.log(response);
+				for(var i=0, j=response.json.length; i<j; i++) {
+					var json =response.json[i];
+				$('' + 
+					'<div id="jTitle">' +
+						'<p>'+ "First Name"+ json.fname +'</p>'+
+						'<p>'+ "Lat Name" +json.lname +'</p>'+
+						'<p>'+ "Password"+json.pword +'</p>'+
+						'<p>'+ "Confirm Password"+json.cpword +'</p>'+
+						'<p>'+ "E-mail"+ json.email +'</p>'+
+						'<p>'+ "deviceValue"+json.deviceValue +'</p>'+
+						'<p>'+ "Friends"+json.friends +'</p>'+
+						'<p>'+ "date"+json.date +'</p>'+
+					'</div>'
+				).appendTo('#jsonData');
+			}
 		}
-	}
-}
-
-//Get the checkbox data
-
-	function getCheckboxValue(){
-		if(go('check').checked){
-				selectValue= go('check').value;
-		
-		}else{
-			selectValue="No"
-	}
-}
-
-//Pick a theme for the groups
-
-	function pickATheme(){
-		var formTag = document.getElementsByTagName("groups");
-			selectLi = go('select');
-			makeSelect = document.createElement('select');
-			makeSelect.setAttribute("id", "groups");
-	for (var i=0, j=contactGroups.length; i<j; i++){
-		var makeOption = document.createElement('option');
-		var optText= contactGroups[i];
-			makeOption.setAttribute("value", optText);
-			makeOption.innerHTML =optText;
-			makeSelect.appendChild(makeOption);
-			
-	}
-			
-			selectLi.appendChild(makeSelect);
-}	
-				
-//Toggle control 
-
-	function toggleControls(n){
-		switch(n) {
-			case"on":
-			go('contactForm').style.display ="none";
-			go('clear').style.display ="inline";
-			go('displayLink').style.display ="none";
-			go('addNew').style.display ="inline";
-			break;
-		case "off":
-			go('contactForm').style.display ="block";
-			go('clear').style.display ="inline";
-			go('displayLink').style.display ="inline";
-			go('addNew').style.display ="none";
-			go('items').style.display="none";
-			break;
-		default:
-		return false;
-	}
-}
-
-//Get data
-
-	function getData(){
-		toggleControls("on");
-	
-		if(localStorage.length === 0){
-				alert("There's no Data in Local Storage so default data was entered");
-				autoFillData();
-}
-		var makeDiv = document.createElement('div');
-			makeDiv.setAttribute("id", "items");
-		var makeList = document.createElement('ul');
-			makeDiv.appendChild(makeList);
-			document.body.appendChild(makeDiv);
-			go('items').style.display ="block";
-	for (var i=0, len=localStorage.length; i<len;i++){
-		var makeli =document.createElement('li');
-		var linksLi= document.createElement('li');
-			makeList.appendChild(makeli);
-		var key=localStorage.key(i);
-		var value=localStorage.getItem(key);
-		var obj=JSON.parse(value);
-		var makeSubList=document.createElement('ul');
-			makeli.appendChild(makeSubList);
-			getImage(obj.group[1], makeSubList);
-		for (var n in obj){
-			var makeSubli=document.createElement('li');
-			makeSubList.appendChild(makeSubli);
-			var optSubText=obj[n][0] + " " + obj[n][1];
-				makeSubli.innerHTML=optSubText;
-				makeSubList.appendChild(linksLi);
-		}
-		makeItemLinks(localStorage.key(i), linksLi); 
-	}
-}
-	
- 
-//Store data
-
-	function storeData(key){
-		if(!key){
-		var id=Math.floor(Math.random()*100000001);
-		
-		}else if(key === "A-Z" || "a-z")
-{
-		localStorage.removeItem(this.key);
-}
-		else
-{
-		id =key;
-}
-		getSelectedRadio();
-		getCheckboxValue();
-		
-		var item            ={};
-			item.groups		=["groups:", go('groups').value];
-			item.fname		=["First Name:", go('fname').value];
-			item.lname		=["Last Name:", go('lname').value];
-			item.pword		=["Password:", go('pword').value];
-			item.cpword		=["Confirm Password:", go('cpword').value];
-			item.email		=["Email:", go('email').value];
-			item.selectValue=["What will you use Wolflinx for?:",selectValue];
-			item.deviceValue=["What device are you using?:",deviceValue];
-			item.friends    =["I have:", go('quantity').value]
-			item.date		=["Date:", go('date').value];		
-			localStorage.setItem(id, JSON.stringify(item));
-			alert("Information is saved!");		
-		
-} 
- 
- 
-//Auto populate Local Storage
-//Store the JSON object in local storage
-    
-    function autoFillData(){
-    	for (var n in json){
-    		var id = Math.floor(Math.random() * 100000001);
-    			localStorage.setItem(id, JSON.stringify(json[n]));
-    	}
-    }
-//links for the items
-	
-	function makeItemLinks(key, linksLi){
-		var breakTag = document.createElement('br');
-		var editLink=document.createElement('a');
-			editLink.href ="#contactForm";
-			editLink.key = key;
-		var editText ="editContact";
-			editLink.addEventListener("click", editItem);
-			editLink.innerHTML = editText;
-			linksLi.appendChild(editLink);
-		
-		var breakTag =document.createElement('br');
-			linksLi.appendChild(breakTag);
-		
-		
-		var deleteLink=document.createElement('a');
-			deleteLink.href="#clear";
-			deleteLink.key =key;
-		var deleteText ="deleteContact";
-			deleteLink.addEventListener("click", deleteItem);
-			deleteLink.innerHTML=deleteText;
-			linksLi.appendChild(deleteLink);
-}
-//edit item
-	
-	function editItem(){
-		var value = localStorage.getItem(this.key);
-		var item = JSON.parse(value);
-			toggleControls("off");
-			go('groups').value=item.group[1];
-			go('fname').value=item.fname[1];
-			go('lname').value=item.lname[1];
-			go('pword').value=item.pword[1];
-			go('cpword').value=item.cpword[1];
-			go('email').value=item.email[1];
-			go('selectValue').value=item.selectValue[1];
-			go('deviceValue').value=item.deviceValue[1];
-			go('friends').value=item.friends[1];
-			go('date').value=item.date[1];
-		var radio=document.forms[0].deviceValue;
-		for (var i=0; i<radio.length; i++){
-			if(radio[i].value == "CellPhone" && item.deviceValue[1] =="CellPhone"){
-				radio[i].setAttribute("checked","checked");
-			}else if(radio[i].value =="Tablet" && item.deviceValue[i] == "Tablet") {
-				radio[i].setAttribute("checked","checked");
-	}
-}
-		if(item.text[1] =="Yes")     {
-			go('check').setAttribute("checked","checked");
-	}
-			go('friends').value=item.friends[1];
-			go('date').value=item.date[1];			
-				
-				save.removeEventListener("click", storeData);
-		
-			go('submit').value ="editContact";
-		var editSubmit = go('submit');
-			editSubmit.addEventListener("click", storeData);
-			editSubmit.key = this.key;
-	
-}
-
-/*Delete Item*/
-	function deleteItem(){
-		var ask = confirm("R U sure U want 2 delete the content?");
-		if(ask){
-			localStorage.removeItem(this.key);
-				alert("Deleted successfully!");
-					window.location.reload();
-		
-		}else{
-			alert("Content not deleted")
-		
-	}
-}
-
+  });
+});
+					
 
 //Clear local storage
-	
-	function clearLocal(){
+function clearLocal(){
 		if(localStorage.length === 0){
 			alert("There's no data to clear!");
 		}else{
@@ -267,82 +114,205 @@ $('#signupPage').on('pageinit', function(){
 				window.location.reload();
 					return false;
 	}
-}	
+};	
 
-//Getting image for the right category
-
-	function getImage(pickATheme, makeSubList){
-    	var imageLi=document.createElement('li'); 
-    		makeSubList.appendChild(imageLi);
-    	var newImg=document.createElement('img');
-    	var setSrc=newImg.setAttribute("src", "images/" + pickATheme + ".png");
-    		imageLi.appendChild(newImg);
-    }
-
-/*Date*/
-	function date(i){
-		var e = document.getElementById('day');
-			while(e.length>0)
-				e.remove(e.length-1);
-		var j=-1;
-		if(i=="na")
-			k=0;
-		else if(i==2)
-			k=28;
-		else if(i==4||i==6||i==9||i==11)
-			k=30;
-		else
-			k=31;
-		while(j++<k){
-		var s=document.createElement('option');
-		var e=document.getElementById('day');
-		if(j==0){
-			s.text="Day";
-			s.value="na";
-		try{
-			e.add(s,null);}
-		catch(ex){
-			e.add(s);}}
-		else{
-			s.text=j;
-			s.value=j;
-		try{
-			e.add(s,null);}
-		catch(ex){
-			e.add(s);}}}}
-			y = 2010;
-		while (y-->1909){
-		var s = document.createElement('option');
-		var e = document.getElementById('year');
-			s.text=y;
-			s.value=y;
-		try{
-			e.add(s,null);}
-			catch(ex){
-		e.add(s);}}
-
-//Local variables and function calls
-		
-		var contactGroups =["--pickATheme--", "Fun", "Education", "Work"],
-			deviceValue,				
-			selectValue = "No",
-			errMsg = go('errors');
-		
-		
-			pickATheme();
 
 //Set Link and Submit Click Events
-	
-		var displayLink = go('displayLink');
-			displayLink.addEventListener("click", getData);
-		var clearLink =go('clear');
-			clearLink.addEventListener("click", clearLocal);
-		var save= go('submit');
-			save.addEventListener("click", storeData);
-
-
+var displayLink = $('displayLink');
+			displayLink.on("click", getData);
+			alert("Displayed");
+		var clearLink =$('#clear');
+			clearLink.on("click", clearLocal);
+		
+		var save= $('#submit');
+			save.on("click", validate);
 });
 
+//Get data
+var getData= function(){
+	
+		if(localStorage.length === 0){
+				alert("There's no Data in Local Storage so default data was entered");
+				autoFillData();
+	}
+		
+		
+		var makeDiv = $('<div id="items"></div>');
+			makeDiv.append('#showMembers');
+		var makeList = $('<ul>');
+			makeList.append("members").append('#item');
+	
+		for (var i=0, len=localStorage.length; i<len;i++){
+		var eachMember =$('<li>');
+		eachMember.append('members');
+		var linksLi= $('<li>');
+		var key=localStorage.key(i);
+		var value=localStorage.getItem(key);
+		var obj=JSON.parse(value);
+		var makeSubList=$('<ul>');
+			makeSubList.append('#eachMember');
+			getImage(obj.member[1], makeSubList);
+		for (var n in obj){
+			var makeSubLi=$('<li>');
+			var optSubText=obj[n][0] + " " + obj[n][1];
+				makeSubli.append('#item')
+					.HTML(optSubText);
+				linkLi.append('#item');
+	
+		}
+		makeItemLinks(localStorage.key(i), linksLi); 
+	makeList.append(linksLi);
+	}
+};
+	
+//Store data
+var storeData= function(data){
+			var id=Math.floor(Math.random()*100000001);
+		console.log(data);
+		deviceValues();	
+		var item            ={};
+			item.fname		=['First Name:', $('#fname').value];
+			item.lname		=['Last Name:', $('#lname').value];
+			item.pword		=['Password:', $('#pword').value];
+			item.cpword		=['Confirm Password:', $('#cpword').value];
+			item.email		=['Email:', $('#email').value];
+			item.deviceValue=['What device are you using?:',deviceValue];
+			item.friends    =['I have:', $('#quantity').value];
+			item.date		=['Date:', $('#date').value];		
+			
+				localStorage.setItem(id, JSON.stringify(item));
+					alert("Information is saved!");	
+					
+		
+} 
+ var storeData = function(data){
+       var id = Math.floor(Math.random()*10000001);
+            id = key;
+        };*/
+        console.log(data);
+        getSelectedRadio();
+        var item= {};
+            item.fname= ['Chore Type:', $('#choretype').val()];
+            item.chorename = ['Chore Name:', $('#chorename').val()];
+            item.finishby  = ['Finish By:', $('#finishby').val()];
+           // item.urgency   = ['Is this chore Urgent?:', getSelectedRadios()];
+            item.difficulty= ["Difficulty:", $('#difficulty').val()];
+            item.recurring = ["Is this a recurring chore?:", $('#recurring').val()];
+            item.chorenotes= ["Chore Notes:", $('#chorenotes').val()];
+            
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("Chore Saved");
+        changePage('displayList');
+        getData();
+
+		console.log('storeData works');
+    }
+
+
+
+
+//Links for the items
+var	makeItemLinks = function(key, linksLi){
+		var editLink=$('a');
+			editLink.attr("href","#signupPage");
+			editLink.key = key;
+		var editText ="Edit Contact";
+			editLink.addClass("editLink").on('click', editItem) .html(editText);
+			linksLi.append(editLink);
+	
+		var deleteLink=$('a');
+			deleteLink.attr("href", "#");
+			deleteLink.key =key;
+		var deleteText ="Delete Contact";
+			deleteLink.attr("deleteLink").on('click', deleteItem).html(deleteText);
+			linksLi.append(deleteLink);
+};
+
+//Edit item
+var editItem= function(){
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+			$('#groups').val(item.groups[1]);
+			$('#fname').val(item.fname[1]);
+			$('#lname').val(item.lname[1]);
+			$('#pword').val(item.pword[1]);
+			$('#cpword').val(item.cpword[1]);
+			$('#email').val(item.email[1]);
+			$('#deviceValue').val(item.deviceValue[1]);
+			$('#friends').val(item.friends[1]);
+  			$('#friends').val(item.friends[1]);
+			$('#date').val(item.date[1]);			
+  		var radios=$('#deviceValue').val();
+		for (var i=0; i<radios.length; i++){
+			if(radios[i].val() == "CellPhone" && item.deviceValue[1] =="CellPhone"){
+				radios[i].attr("checked","checked");
+			}else if($(radios[i]).val() =="Tablet" && item.deviceValue[i] == "Tablet"){
+				radios[i].attr("checked","checked");
+			}
+}
+		
+			var editSubmit = $('#submit');
+			editSubmit.off("click", validate);
+			editSubmit.val("editContact");
+			editSubmit.on("click", storeData);
+			editSubmit.key = this.key;
+};
+		
+//Delete Item
+var deleteItem = function (){
+		var ask = confirm("R U sure U want 2 delete the content?");
+		if(ask){
+			localStorage.removeItem(this.key);
+				alert("Deleted successfully!");
+					window.location.reload();
+}
+};
+
+//Auto-populate Local Storage and
+//store the JSON object in local storage
+var autoFillData= function (){
+    	for (var n in json){
+    		var id = Math.floor(Math.random()*100000001);
+    			localStorage.setItem(id, JSON.stringify(json[n]));
+    	alert("It filled the data");
+    	}
+    };
+
+
+//Getting image for the right category
+var getImage= function (pickATheme, makeSubList){
+		var imageLi=$('li'); 
+    		makeSubList.append(imageLi);
+    	var newImg=$('img');
+    	var setSrc=newImg.attr("src", "images/" + pickATheme + ".png");
+    		imageLi.append(newImg);
+    };
+
+
+//Get the radio button data			
+var deviceValues = function() {
+        var radios = $('input:radio[name=deviceValue]:checked').val();
+			return radios;
+};
+
+
+
+
+//Laod the XML
+
+<?xml version="1.0" encoding="UTF-8"?>
+	
+
+
+
+
+
+
+
+
+
+
+   
 
 
 
